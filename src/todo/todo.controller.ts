c97e6378ba6137e929b2a654a7f332720501a397
nargok <nargok@gmail.com>
2023-05-06 15:37:38 +0900

    todo api

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,62 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { AuthGuard } from '@nestjs/passport';
+import { Task } from '@prisma/client';
+import { Request } from 'express';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  getTasks(@Req() req: Request): Promise<Task[]> {
+    return this.todoService.getTasks(req.user.id);
+  }
+
+  @Get(':id')
+  getTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<Task> {
+    return this.todoService.getTaskById(req.user.id, taskId);
+  }
+
+  @Post()
+  createTask(@Req() req: Request, @Body() dto: CreateTaskDto): Promise<Task> {
+    return this.todoService.createTask(req.user.id, dto);
+  }
+
+  @Patch(':id')
+  updateTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+    @Body() dto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.todoService.updateTask(req.user.id, taskId, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<void> {
+    return this.todoService.deleteTask(req.user.id, taskId);
+  }
+}
